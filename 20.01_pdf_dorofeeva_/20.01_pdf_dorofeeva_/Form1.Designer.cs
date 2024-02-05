eaa6d712a4aa191ff482516962ef3094d7c6e6a6
leisan08dor <129215656+leisan08dor@users.noreply.github.com>
2024-02-05 13:47:21 +0300

    Add files via upload

---
+++ 20.01_pdf_dorofeeva_/20.01_pdf_dorofeeva_/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace _20._01_pdf_dorofeeva_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.button1.Location = new System.Drawing.Point(256, 353);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Печать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 31);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(512, 289);
+            this.textBox1.TabIndex = 1;
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.Document = this.printDocument1;
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(587, 416);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "PrintToPDF";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+    }
+}
+
---
+++ 20.01_pdf_dorofeeva_/20.01_pdf_dorofeeva_/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20._01_pdf_dorofeeva_
+{
+    public partial class Form1 : Form
+    {
+        // создали экземпляры объектов, с к-ми дальше взаимодействуем
+        PrintDocument document = new PrintDocument();
+        PrintDialog dialog = new PrintDialog();
+
+        public Form1()
+        {
+            InitializeComponent();
+            // явно указали в обработчике событий печати,что печатать необходимо метод document_PrintPage
+            printDocument1.PrintPage += new PrintPageEventHandler(document_PrintPage);
+        }
+
+        void document_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            // метод Graphics.DrawString создает заданную текстовую строку в указанном месте на странице и формате 
+            e.Graphics.DrawString(textBox1.Text, new Font("Times New Roman", 14, FontStyle.Regular), Brushes.Black, 20, 20);
+        }
+
+        // обработчик событий кнопки
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // вызвали диалог печати документа
+            dialog.Document = document;
+            // при подтверждении, файл отправляется в печать
+            if (printDialog1.ShowDialog() == DialogResult.OK)
+                printDocument1.Print();
+        }
+    }
+}
